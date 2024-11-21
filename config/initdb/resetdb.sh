6d7e234446926f50bc9dd8e46890f8ac3f73be0f
Herbert Balagtas <hbalagta@uwaterloo.ca>
2024-11-20 21:27:25 -0500

    update resetdb script

--- config/initdb/resetdb.sh
+++ config/initdb/resetdb.sh
@@ -5,5 +5,5 @@ MYSQL_PASSWORD="docker"
 
 # Execute MySQL commands
 mysql -u root -p"${MYSQL_PASSWORD}" -e "DROP DATABASE IF EXISTS docker; CREATE DATABASE docker;"
-mysql -u root -p"${MYSQL_PASSWORD}" docker < docker.sql
+mysql -u root -p"${MYSQL_PASSWORD}" docker < /docker-entrypoint-initdb.d/docker.sql
 
