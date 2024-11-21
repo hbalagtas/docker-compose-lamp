7f7345123f02b9d92edee8ae9bbbd2d49d733f03
Herbert Balagtas <hbalagta@uwaterloo.ca>
2024-11-20 21:19:13 -0500

    Add script to reset the database with original docker.sql file

---
+++ config/initdb/resetdb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# MySQL credentials
+MYSQL_PASSWORD="docker"
+
+# Execute MySQL commands
+mysql -u root -p"${MYSQL_PASSWORD}" -e "DROP DATABASE IF EXISTS docker; CREATE DATABASE docker;"
+mysql -u root -p"${MYSQL_PASSWORD}" docker < docker.sql
+
